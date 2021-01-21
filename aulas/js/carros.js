1163293a4c8b861868a120fa73981c83e760e7b3
ivanluis <ivanlfsilva@ufrn.edu.br>
2021-01-21 15:07:34 -0500

    formatter

--- aulas/js/carros.js
+++ aulas/js/carros.js
@@ -1,26 +1,25 @@
 var Carros = /** @class */ (function () {
-    function Carros() {
-    }
-    Carros.prototype.getModelo = function () {
-        return this.modelo;
-    };
-    Carros.prototype.setModelo = function (modelo) {
-        this.modelo = modelo;
-    };
-    Carros.prototype.getPlaca = function () {
-        return this.placa;
-    };
-    Carros.prototype.setPlaca = function (placa) {
-        this.placa = placa;
-    };
-    Carros.prototype.getKilometragem = function () {
-        return this.kilometragem;
-    };
-    Carros.prototype.setKilometragem = function (kilometragem) {
-        this.kilometragem = kilometragem;
-    };
-    return Carros;
-}());
+  function Carros() {}
+  Carros.prototype.getModelo = function () {
+    return this.modelo;
+  };
+  Carros.prototype.setModelo = function (modelo) {
+    this.modelo = modelo;
+  };
+  Carros.prototype.getPlaca = function () {
+    return this.placa;
+  };
+  Carros.prototype.setPlaca = function (placa) {
+    this.placa = placa;
+  };
+  Carros.prototype.getKilometragem = function () {
+    return this.kilometragem;
+  };
+  Carros.prototype.setKilometragem = function (kilometragem) {
+    this.kilometragem = kilometragem;
+  };
+  return Carros;
+})();
 var fiat147 = new Carros();
 fiat147.setModelo("fiat 500");
 console.log(fiat147.getModelo());
--- aulas/ts/carros.ts
+++ aulas/ts/carros.ts
@@ -3,36 +3,35 @@ class Carros {
   private placa: string;
   private kilometragem: string;
 
-    public getModelo(): string {
-        return this.modelo;
-    }
+  public getModelo(): string {
+    return this.modelo;
+  }
 
-    public setModelo(modelo: string): void {
-        this.modelo = modelo;
-    }
+  public setModelo(modelo: string): void {
+    this.modelo = modelo;
+  }
 
-    public getPlaca(): string {
-        return this.placa;
-    }
+  public getPlaca(): string {
+    return this.placa;
+  }
 
-    public setPlaca(placa: string): void {
-        this.placa = placa;
-    }
+  public setPlaca(placa: string): void {
+    this.placa = placa;
+  }
 
-    public getKilometragem(): string {
-        return this.kilometragem;
-    }
+  public getKilometragem(): string {
+    return this.kilometragem;
+  }
 
-    public setKilometragem(kilometragem: string): void {
-        this.kilometragem = kilometragem;
-    }
-
+  public setKilometragem(kilometragem: string): void {
+    this.kilometragem = kilometragem;
+  }
 }
 
-var fiat147 = new Carros()
-fiat147.setModelo("fiat 500")
-console.log(fiat147.getModelo())
-fiat147.setPlaca("MKV-G786")
-console.log(fiat147.getPlaca())
-fiat147.setKilometragem("35.000 KM")
-console.log(fiat147.getKilometragem())
+var fiat147 = new Carros();
+fiat147.setModelo("fiat 500");
+console.log(fiat147.getModelo());
+fiat147.setPlaca("MKV-G786");
+console.log(fiat147.getPlaca());
+fiat147.setKilometragem("35.000 KM");
+console.log(fiat147.getKilometragem());
